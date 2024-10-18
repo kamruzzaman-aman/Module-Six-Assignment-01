d02649b6d99c3f700c7660687a2feacf77401313
KAMRUZZAMAN <kamrulcse1@gmail.com>
2024-10-18 23:14:48 +0600

    last revision

--- student_result/result.php
+++ student_result/result.php
@@ -1,61 +1,4 @@
- <?php
-    // function resultFunc($subject1, $subject2, $subject3, $subject4, $subject5){
-    //    $rangeMessage = "Mark range is invalid.";
-    //    $failedMessage = "You have failed";
-    //     if ($subject1<0 || $subject1>100) {
-    //         print($rangeMessage);
-    //     }
-    //     else if ($subject2<0 || $subject2>100) {
-    //         print($rangeMessage);
-    //     }
-    //     else if ($subject3<0 || $subject3>100) {
-    //         print($rangeMessage);
-    //     }
-    //     else if ($subject4<0 || $subject4>100) {
-    //         print($rangeMessage);
-    //     }
-    //     else if ($subject5<0 || $subject5>100) {
-    //         print($rangeMessage);
-    //     }
-    //     else{
-    //         if ($subject1 < 33 || $subject2 < 33 || $subject3 < 33 || $subject4 < 33 || $subject5 < 33) {
-    //             print($failedMessage);
-    //         }
-    //         else{
-    //             $totalMarks = $subject1+$subject2+$subject3+$subject4+$subject5;
-    //             $averageMarks = $totalMarks/5;
-    //             print("Total Marks: ".$totalMarks."\n");
-    //             print("Average Marks: ".$averageMarks."\n");
-    //             switch($averageMarks){
-    //                 case ($averageMarks>=80 && $averageMarks<=100):
-    //                     print("Grade: A+");
-    //                     break;
-    //                 case ($averageMarks>=70 && $averageMarks<=79):
-    //                     print("Grade: A");
-    //                     break;
-    //                 case ($averageMarks>=60 && $averageMarks<=69):
-    //                     print("Grade: A-");
-    //                     break;
-    //                 case ($averageMarks>=50 && $averageMarks<=59):
-    //                     print("Grade: B");
-    //                     break;
-    //                 case ($averageMarks>=40 && $averageMarks<=49):
-    //                     print("Grade: C");
-    //                     break;
-    //                 case ($averageMarks>=33 && $averageMarks<=39):
-    //                     print("Grade: D");
-    //                     break;
-    //                 default:
-    //                     print("Grade: F");
-    //             }
-    //         }
-    //     }
-    // }
-
-    // resultFunc(80, 90, 50, 60, 55); 
-
-
-
+<?php
     function resultFunc($s1, $s2, $s3, $s4, $s5)
     {
         $rangeMessage = "Mark range is invalid.";
@@ -126,4 +69,4 @@
         }
     }
     
-    resultFunc(90, 90, 10, 60, 55);
+    resultFunc(90, 90, 55, 60, 55);
